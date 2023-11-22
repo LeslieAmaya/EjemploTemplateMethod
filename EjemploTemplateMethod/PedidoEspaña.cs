4741d8436e2dedf1742409b1bcd782563e34f97d
iza02 <iza02@DESKTOP-54I51SF>
2023-11-21 18:58:17 -0700

    Agregar archivos de proyecto.

---
+++ EjemploTemplateMethod/PedidoEspaña.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploTemplateMethod
+{
+    public class PedidoEspaña : Pedido
+    {
+        protected override void calculaIVA()
+        {
+            importeIVA = importeSinIVA * 0.2;
+        }
+    }
+}
---
+++ EjemploTemplateMethod/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploTemplateMethod
+{
+    public class Usuario
+    {
+        static void Main(string[] args)
+        {
+            Pedido pedidoEspaña = new PedidoEspaña();
+            pedidoEspaña.setImporteSinIVA(10000);
+            pedidoEspaña.calculaPrecioConIVA();
+            pedidoEspaña.visualiza();
+
+
+            Pedido pedidoLuxemburgo = new PedidoLuxemburgo();
+            pedidoLuxemburgo.setImporteSinIVA(10000);
+            pedidoLuxemburgo.calculaPrecioConIVA();
+            pedidoLuxemburgo.visualiza();
+        }
+    }
+}
